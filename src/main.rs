11f2c04aab70248881e3fddfb7b75a5e1e6b27d9
Vadim <grozinok@gmail.com>
2022-09-29 11:24:14 +0200

    wip: add clap

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,22 @@ use anchor_lang::InstructionData;
 use anchor_lang::ToAccountMetas;
 use borsh::{BorshDeserialize, BorshSchema, BorshSerialize};
 use serde::{Deserialize, Serialize};
-use solana_program::instruction::{AccountMeta, Instruction};
-use solana_sdk::{pubkey::Pubkey, system_program, sysvar::rent};
+use solana_sdk::{pubkey::Pubkey, system_program, sysvar::rent, instruction::{AccountMeta, Instruction}};
 use voter_stake_registry::state::LockupKind;
+use clap::{arg, command, value_parser, ArgAction, Command};
+use solana_remote_wallet::locator::Locator;
+use solana_remote_wallet::remote_keypair::generate_remote_keypair;
+use solana_remote_wallet::remote_wallet::maybe_wallet_manager;
+use solana_sdk::derivation_path::DerivationPath;
+use solana_sdk::{
+    self, commitment_config::CommitmentConfig, signature::Keypair, signature::Signer,
+    transaction::Transaction as SolanaTransaction, signer::keypair::read_keypair_file
+};
+use uriparse::URIReference;
 
 use dotenv::dotenv;
 
-use std::{env, fs, str::FromStr};
+use std::{env, fs, str::FromStr, path::PathBuf};
 
 #[derive(Clone, Debug, PartialEq, BorshDeserialize, BorshSerialize, BorshSchema)]
 #[repr(C)]
@@ -99,6 +108,13 @@ pub struct Grant {
     pub amount: u64,
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct ProposalData {
+    pub name: String,
+    pub description: String,
+    pub grants: Vec<Grant>,
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Transaction {
     pub wallet: String,
@@ -110,24 +126,89 @@ pub struct Transaction {
     pub tx: String,
 }
 
-pub type Grants = Vec<Grant>;
 pub type Transactions = Vec<Transaction>;
 
 fn main() {
     dotenv().ok();
 
-    let grants_data = fs::read_to_string("./grants.json").unwrap();
+    // let grants_data = fs::read_to_string("./grants.json").unwrap();
 
-    let grants: Grants = serde_json::from_str(&grants_data).unwrap();
+    // let grants: Grants = serde_json::from_str(&grants_data).unwrap();
 
-    let transactions = grant_transactions(grants);
+    // let transactions = grant_transactions(grants);
 
-    let j = serde_json::to_string(&transactions).unwrap();
+    // let j = serde_json::to_string(&transactions).unwrap();
 
-    fs::write("./transactions.json", j).unwrap();
+    // fs::write("./transactions.json", j).unwrap();
+
+    let matches = command!()
+        .arg(
+            arg!(
+                -w --wallet <FILE> "Fee payer wallet"
+            )
+            .value_parser(value_parser!(PathBuf)),
+        )
+        .subcommand(
+            Command::new("grant")
+                .about("creates new DAO proposal and attaches bunch of Grant transactions to it")
+                .arg(arg!(-g --grants "lists of grants to be created").required(true).action(ArgAction::Set))
+        )
+        .get_matches();
+
+    let wallet_path = matches.get_one::<PathBuf>("wallet").unwrap();
+
+    if let Some(matches) = matches.subcommand_matches("grant") {
+        let grants_file = matches.get_one::<String>("grants").unwrap();
+
+        let grants_data = fs::read_to_string(grants_file).unwrap();
+
+        let grants: ProposalData = serde_json::from_str(&grants_data).unwrap();
+    }
 }
 
-pub fn grant_transactions(grants: Grants) -> Transactions {
+fn create_proposal(name: String, description: String, grants: Vec<Instruction>) {
+    // to get proposal_index call RPC to get governance_data.proposals_count
+    // proposal_owner_record - Account PDA seeds: ['governance', realm, token_mint, token_owner ]
+    // governance_auth will be the wallet
+
+}
+
+fn keypair_or_ledger_of(path: &PathBuf) -> Option<Box<dyn Signer>> {
+    return if path.starts_with("usb://") {
+        let uri_invalid_msg =
+            "Failed to parse usb:// keypair path. It must be of the form 'usb://ledger?key=0'.";
+        let uri_ref = URIReference::try_from(path.to_str().unwrap()).expect(uri_invalid_msg);
+        let derivation_path = DerivationPath::from_uri_key_query(&uri_ref)
+            .expect(uri_invalid_msg)
+            .unwrap_or_default();
+        let locator = Locator::new_from_uri(&uri_ref).expect(uri_invalid_msg);
+
+        let hw_wallet = maybe_wallet_manager()
+            .expect("Remote wallet found, but failed to establish protocol. Maybe the Solana app is not open.")
+            .expect("Failed to find a remote wallet, maybe Ledger is not connected or locked.");
+
+        // When using a Ledger hardware wallet, confirm the public key of the
+        // key to sign with on its display, so users can be sure that they
+        // selected the right key.
+        let confirm_public_key = true;
+
+        Some(Box::new(
+            generate_remote_keypair(
+                locator,
+                derivation_path,
+                &hw_wallet,
+                confirm_public_key,
+                "council", /* When multiple wal
+                            lets are connected, used to display a hint */
+            )
+            .expect("Failed to contact remote wallet"),
+        ))
+    } else {
+        Some(Box::new(read_keypair_file(path.to_str().unwrap()).unwrap()))
+    };
+}
+
+pub fn grant_transactions(grants: Vec<Grant>) -> Vec<Instruction> {
     let voter_stake_program = Pubkey::from_str(&env::var("VOTER_STAKE_PROGRAM").unwrap()).unwrap();
 
     let mint = Pubkey::from_str(&env::var("MINT").unwrap()).unwrap();
@@ -142,7 +223,7 @@ pub fn grant_transactions(grants: Grants) -> Transactions {
 
     let payer = Pubkey::from_str(&env::var("PAYER").unwrap()).unwrap();
 
-    let mut transactions = Vec::new();
+    let mut instructions = Vec::new();
 
     for grant in grants.iter() {
         // wallet
@@ -197,28 +278,14 @@ pub fn grant_transactions(grants: Grants) -> Transactions {
         }
         .data();
 
-        let instruction: LocalInstructionData = Instruction {
+        let instruction = Instruction {
             program_id: voter_stake_program,
             data,
             accounts,
-        }
-        .into();
+        };
 
-        let mut instruction_bytes = vec![];
-        instruction.serialize(&mut instruction_bytes).unwrap();
-
-        let encoded_instruction = base64::encode(instruction_bytes);
-
-        transactions.push(Transaction {
-            wallet: grant.wallet.clone(),
-            grant_type: grant.grant_type.clone(),
-            start: grant.start,
-            periods: grant.periods,
-            allow_clawback: grant.allow_clawback,
-            amount: grant.amount,
-            tx: encoded_instruction,
-        });
+        instructions.push(instruction);
     }
 
-    transactions
+    instructions
 }
